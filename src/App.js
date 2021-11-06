fc853318a1e1afaab95d9eeed2c3764f3260c7c6
Josélio <joseliobackup@gmail.com>
2021-11-06 16:57:31 -0300

    remaster project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// Utilities
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
+import { Site, Delimiters } from './components/MainComponets';
+
+// Partial components
+import Header from './components/partials/Header';
+
+// pages
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Site>
+        <Header />
+        <Delimiters>
+          <Routes />
+        </Delimiters>
+      </Site>
+    </BrowserRouter>
   );
 }
 
