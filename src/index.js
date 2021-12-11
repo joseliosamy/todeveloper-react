57c71ab287ed0b563c5e74a620ef663d82d17a14
Josélio <joseliobackup@gmail.com>
2021-12-10 21:56:40 -0300

    fix bug redux

--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import Reducers from './reducers';
 import './global.css';
 
-const store = createStore(Reducers);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
