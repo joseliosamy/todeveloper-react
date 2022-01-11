063d82119bf96cf80cdbf40c0a4539cb10506b9b
Josélio <joseliobackup@gmail.com>
2022-01-11 01:12:41 -0300

    update domain

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 import { Site } from './components/MainComponets';
-
+//ok
 // Partial components
 import Header from './components/partials/Header';
 
